3087d5527b6afe09344077ea2a577d75f5b09b6f
Ramadan Mohamed <81324838+RamadanMohamed-fd@users.noreply.github.com>
2023-02-27 21:36:26 +0200

    commit change 3

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -31,7 +31,7 @@ const Contact = () => {
               <i className="bx bx-mail-send contact__card-icon"></i>
 
               <h3 className="contact__card-title">Email</h3>
-              <span className="contact__card-data">user@gmail.com</span>
+              <span className="contact__card-data">futurezorro@gmail.com</span>
 
               <a
                 href="mailto:examplemail@gmail.com.com"
@@ -49,7 +49,7 @@ const Contact = () => {
               <i className="bx bxl-whatsapp contact__card-icon"></i>
 
               <h3 className="contact__card-title">Whatsapp</h3>
-              <span className="contact__card-data">+02 999-888-777</span>
+              <span className="contact__card-data">01003231595</span>
 
               <a
                 href="https://api.whatsapp.com/send?phone=62214408789&text=Hello, more information!"
@@ -68,7 +68,7 @@ const Contact = () => {
               <i className="bx bxl-messenger contact__card-icon"></i>
 
               <h3 className="contact__card-title">Messanger</h3>
-              <span className="contact__card-data">user@gmail.com</span>
+              <span className="contact__card-data">https://www.facebook.com/profile.php?id=100022061774566</span>
 
               <a href="https://m.me/crypticalcoder" className="contact__button">
                 Write me
